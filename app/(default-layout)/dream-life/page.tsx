9e604271e41805b658cd9b29b3467337368815f3
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2023-12-18 15:13:40 -0600

    GM-849 resolved build issues.

--- app/(default-layout)/dream-life/page.tsx
+++ app/(default-layout)/dream-life/page.tsx
@@ -8,20 +8,17 @@ import { Image } from '@/components/Image'
 import { Text } from '@/components/Text/Text'
 import { TestimonialSection } from '@/components/TestimonialSection'
 import { Page } from '@/components/Page'
-import { Icon } from '@/components/Icon'
-import { Input } from '@/components/Input/Input'
-import { IDefaultProps } from '@/components'
 import { Video } from '@/components/Video/Video'
+// config
 import { TRIAL_HEADSPACE as TH } from './config'
-//libraries
-import cx from 'classnames'
-import { Form, Formik, FormikHelpers } from 'formik'
-import * as yup from 'yup'
-//modules
-import { EExternalRoutes } from '@/utils/constants'
+// modules
 import Mixpanel from '@/modules/Mixpanel'
 import { Articles } from '@/components/Articles'
-import { CommunityTeaser } from '@/components/CommunityTeaser'
+import { CommunityTeaser, PaymentOptions } from '@/components/CommunityTeaser'
+// utils
+import { EExternalRoutes } from '@/utils/constants'
+
+type ArticleKey = keyof typeof TH.ARTICLES
 
 export default function DreamLifePage() {
   const page_name = '7 Day Free Trial Headspace'
@@ -151,116 +148,3 @@ export default function DreamLifePage() {
     </Page>
   )
 }
-
-interface IPaymentOptionsProps extends IDefaultProps {
-  placement?: string
-  onOptionSelected: (option: string, checked: boolean) => void
-}
-
-export const PaymentOptions = ({
-  className,
-  placement = 'top',
-  onOptionSelected,
-}: IPaymentOptionsProps) => {
-  const onSubmit = (values: any, formikHelpers: FormikHelpers<any>) => {
-    formikHelpers.setSubmitting(false)
-
-    Mixpanel.track.ButtonClicked({
-      button_label: 'TRY FOR FREE',
-      page_name: '7 Day Free Trial Headspace',
-      redirection: values.option1 ? '7-day-trial' : '14-day-trial',
-    })
-
-    window.location.assign(
-      values.option1
-        ? EExternalRoutes.THINKIFIC_CHECKOUT_7_DAY_TRIAL
-        : EExternalRoutes.THINKIFIC_CHECKOUT_14_DAY_TRIAL_QUARTERLY
-    )
-  }
-
-  return (
-    <Formik
-      initialValues={headspaceFormInitialValues}
-      validationSchema={HeadspaceFormValidationSchema}
-      onSubmit={onSubmit}>
-      {({ values, setFieldValue }) => {
-        return (
-          <Form>
-            <div
-              className={cx(
-                `flex flex-col space-y-4 lg:flex-row lg:justify-between items-center`,
-                className
-              )}>
-              {TH.HERO.offers.map((offer, index) => (
-                <label
-                  key={`offer_${index}`}
-                  className={`relative max-w-[474px] flex row items-center space-x-5 ${
-                    // @ts-ignore
-                    values[`option${index + 1}`] ? 'text-white bg-primary' : 'text-black bg-white'
-                  } rounded-[30px] cursor-pointer p-6 lg:px-7 lg:pt-7 lg:pb-5`}
-                  htmlFor={`option_${placement}_${index + 1}`}>
-                  {index == 0 && (
-                    <Text.Paragraph
-                      className="absolute -top-5 font-bold text-black bg-blue rounded-10 py-2 px-4"
-                      content="BEST VALUE"
-                    />
-                  )}
-
-                  <div className="text-left">
-                    <Text.Paragraph
-                      className="font-bold"
-                      content={offer.type}
-                      spacing="tracking-0.325"
-                    />
-
-                    <Text.Heading className="mt-3" content={offer.title} />
-
-                    <Text.Paragraph
-                      className={`max-w-[336px] !text-sm mt-4 lg:block ${
-                        placement === 'bottom' && 'hidden lg:block'
-                      }`}
-                      content={offer.copy}
-                    />
-                  </div>
-
-                  <Input.Checkbox
-                    classNameInput={`!w-7 !h-7 lg:!w-10 lg:!h-10 ${
-                      // @ts-ignore
-                      values[`option${index + 1}`] ? '!bg-black' : '!bg-white'
-                    }`}
-                    name={`option_${placement}_${index + 1}`}
-                    // @ts-ignore
-                    value={values[`option${index + 1}`]}
-                    onChange={() => {
-                      for (let i = 1; i <= TH.HERO.offers.length; i++) {
-                        setFieldValue(`option${i}`, false)
-                        onOptionSelected(`option${i}`, false)
-                      }
-                      setFieldValue(`option${index + 1}`, true)
-                      onOptionSelected(`option${index + 1}`, true)
-                    }}
-                  />
-                </label>
-              ))}
-            </div>
-
-            <Button.Submit
-              className="w-44 !font-bold border-none mt-4 lg:mt-10"
-              label="TRY FOR FREE"
-            />
-          </Form>
-        )
-      }}
-    </Formik>
-  )
-}
-
-const HeadspaceFormValidationSchema = yup
-  .object()
-  .shape({
-    option1: yup.bool().default(true),
-    option2: yup.bool().default(false),
-  })
-  .defined()
-
-const headspaceFormInitialValues = HeadspaceFormValidationSchema.cast({})

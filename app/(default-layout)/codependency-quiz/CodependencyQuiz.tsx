b270db04a5af54ba11f5f67050618a523e1d2048
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2023-12-20 12:50:25 -0600

    GM-854 converted codependency quiz page to SSR. Updated sitemap. Fixed a bug with CarouselQuiz for server rendering. Added client component for Codependency Quiz.

---
+++ app/(default-layout)/codependency-quiz/CodependencyQuiz.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Quiz } from '@/components/Quiz'
+import {
+  CODEPENDENCY_QUIZ_RESULTS,
+  CODEPENDENCY_QUIZ_QUESTIONS,
+  codependencyQuizValidationSchema,
+} from './config'
+
+export const CodependencyQuiz = () => (
+  <Quiz
+    outputs={CODEPENDENCY_QUIZ_RESULTS}
+    questions={CODEPENDENCY_QUIZ_QUESTIONS}
+    theme="blue-lightest"
+    validationSchema={codependencyQuizValidationSchema}
+    quizName="Codependency Quiz"
+  />
+)

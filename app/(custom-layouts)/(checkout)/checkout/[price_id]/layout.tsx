b231a0cfcd60e17657e95cb1a2db93717baa9d50
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2023-12-22 11:13:19 -0600

    PROD-2395 adjusted navigation to include Book. Re-ordered footer links. Added navigation to not-found pages.

--- app/(custom-layouts)/(checkout)/checkout/[price_id]/layout.tsx
+++ app/(custom-layouts)/(checkout)/checkout/[price_id]/layout.tsx
@@ -3,13 +3,7 @@ import { Header } from '@/components/Header'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header
-        includeGoBackButton
-        clickableLogo={false}
-        includeSideMenu={false}
-        goBackButtonText="Cancel"
-        navLinks={[]}
-      />
+      <Header includeGoBackButton clickableLogo={false} goBackButtonText="Cancel" navLinks={[]} />
 
       {children}
     </>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,48 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { NotFound } from '@/components/NotFound'
+import { EExternalRoutes, ERoutes } from '@/utils/constants'
 
 export default function NotFoundPage() {
   return (
     <>
-      <Header />
+      <Header
+        includeSideMenu
+        navLinks={[
+          {
+            link: ERoutes.ATTACHMENT_QUIZ,
+            text: 'Attachment Quiz',
+          },
+          {
+            link: EExternalRoutes.PDS_COURSES,
+            text: 'View Courses',
+          },
+          {
+            link: ERoutes.IAT_SALES_PAGE,
+            text: 'Certication',
+          },
+          {
+            link: EExternalRoutes.COLLECTIONS,
+            text: 'Memberships',
+          },
+          {
+            link: EExternalRoutes.ABOUT,
+            text: 'About',
+          },
+          {
+            link: EExternalRoutes.BLOG,
+            text: 'Blog',
+          },
+          {
+            link: ERoutes.LEARNING_LOVE_PAGE,
+            text: 'Book',
+          },
+        ]}
+      />
 
       <NotFound />
 
-      <Footer />
+      <Footer includeLinks />
     </>
   )
 }

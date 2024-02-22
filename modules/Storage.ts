899641407da59b6c6e94741fe6bd0c67665e4610
rajputpraj <pratikraj@personaldevelopmentschool.com>
2024-02-22 10:46:15 -0500

    GM-866 adjusting for merge

--- modules/Storage.ts
+++ modules/Storage.ts
@@ -7,6 +7,7 @@ export type TStorageKeys =
   | 'prod-2320-checkout-test'
   | 'gm-845-checkout-test'
   | 'seo-90-organic-quiz-test'
+  | 'gm-896-fa-retest'
   | 'gm-866-headline-test-da'
   | 'gm-866-headline-test-sa'
 

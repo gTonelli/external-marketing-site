1139a351b2e010c3c3103484cd0004fedfbb17e3
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2024-04-17 16:07:12 -0500

    PROD-2671 added custom radio input component.

--- components/AttachmentQuizV2/AttachmentQuizV2RadioInput.tsx
+++ components/AttachmentQuizV2/AttachmentQuizV2RadioInput.tsx
@@ -12,7 +12,8 @@ interface IAttachmentQuizV2RadioInputProps {
 export const AttachmentQuizV2RadioInput = ({ number }: IAttachmentQuizV2RadioInputProps) => {
   const { errors, submitCount } = useFormikContext()
   const error = submitCount > 0 && get(errors, `question_${number}`)
-  const defaultInputClasses = 'block !text-primary !border-2 !border-primary lg:cursor-pointer'
+  const defaultInputClasses =
+    'block !border-2 !border-primary cursor-pointer dark bg-white rounded-full hover:bg-primary-light transition-colors mx-auto'
 
   return (
     <div className="relative border-b border-grey">
@@ -20,60 +21,91 @@ export const AttachmentQuizV2RadioInput = ({ number }: IAttachmentQuizV2RadioInp
         className="grid grid-cols-5 gap-4 items-stretch text-center max-w-xs mx-auto mt-4 mb-7 lg:max-w-none lg:grid-cols-7 lg:items-center"
         component="div"
         name={`question_${number}`}>
-        <Input
-          classNameInput={cx(`!w-10 !h-10`, defaultInputClasses)}
-          id={`question_${number}_input_0`}
-          name={`question_${number}`}
-          type="radio"
-          value={0}
-        />
+        <div>
+          <input
+            className="!hidden"
+            id={`question_${number}_input_0`}
+            name={`question_${number}`}
+            type="radio"
+            value={0}
+          />
 
-        <Input
-          classNameInput={cx(`!w-8 !h-8 my-1`, defaultInputClasses)}
-          id={`question_${number}_input_1`}
-          name={`question_${number}`}
-          type="radio"
-          value={0}
-        />
+          <label
+            className={cx('w-10 h-10', defaultInputClasses)}
+            htmlFor={`question_${number}_input_0`}
+          />
+        </div>
 
-        <Input
-          classNameInput={cx(`!w-7 !h-7 my-[6px]`, defaultInputClasses)}
-          id={`question_${number}_input_2`}
-          name={`question_${number}`}
-          type="radio"
-          value={0}
-        />
+        <div>
+          <input
+            className="!hidden"
+            id={`question_${number}_input_1`}
+            name={`question_${number}`}
+            type="radio"
+            value={0}
+          />
 
-        <Input
-          classNameInput={cx(`!w-8 !h-8 my-1`, defaultInputClasses)}
-          id={`question_${number}_input_3`}
-          name={`question_${number}`}
-          type="radio"
-          value={0.5}
-        />
+          <label
+            className={cx('w-8 h-8', defaultInputClasses)}
+            htmlFor={`question_${number}_input_1`}
+          />
+        </div>
 
-        <Input
-          classNameInput={cx(`!w-10 !h-10`, defaultInputClasses)}
-          id={`question_${number}_input_4`}
-          name={`question_${number}`}
-          type="radio"
-          value={1}
-        />
+        <div>
+          <input
+            className="!hidden"
+            id={`question_${number}_input_2`}
+            name={`question_${number}`}
+            type="radio"
+            value={0}
+          />
 
-        <label
-          className="text-sm text-primary lg:cursor-pointer lg:col-start-1 lg:row-start-1 lg:text-left"
-          htmlFor={`question_${number}_input_0`}>
+          <label
+            className={cx('w-7 h-7', defaultInputClasses)}
+            htmlFor={`question_${number}_input_2`}
+          />
+        </div>
+
+        <div>
+          <input
+            className="!hidden"
+            id={`question_${number}_input_3`}
+            name={`question_${number}`}
+            type="radio"
+            value={0}
+          />
+
+          <label
+            className={cx('w-8 h-8', defaultInputClasses)}
+            htmlFor={`question_${number}_input_3`}
+          />
+        </div>
+
+        <div>
+          <input
+            className="!hidden"
+            id={`question_${number}_input_4`}
+            name={`question_${number}`}
+            type="radio"
+            value={0}
+          />
+
+          <label
+            className={cx('w-10 h-10', defaultInputClasses)}
+            htmlFor={`question_${number}_input_4`}
+          />
+        </div>
+
+        <p className="text-sm text-primary mb-0 lg:col-start-1 lg:row-start-1 lg:text-left">
           Disagree
-        </label>
+        </p>
 
-        <label
-          className="text-sm text-primary col-start-5 lg:cursor-pointer lg:col-start-7 lg:row-start-1 lg:text-right"
-          htmlFor={`question_${number}_input_4`}>
+        <p className="text-sm text-primary col-start-5 mb-0 lg:col-start-7 lg:row-start-1 lg:text-right">
           Agree
-        </label>
+        </p>
       </Field>
 
-      {error && <p className="w-full absolute -bottom-5 text-danger text-center mt-1">{error}</p>}
+      {error && <p className="w-full absolute -bottom-4 text-danger text-left mt-1">{error}</p>}
     </div>
   )
 }

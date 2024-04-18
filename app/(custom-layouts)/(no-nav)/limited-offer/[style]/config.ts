8fa2e0dda840c968c1c966588355200c5250fa2e
Alpesh Patel <alpesh@personaldevelopmentschool.com>
2024-04-18 14:55:15 -0400

    GM-962 | add video split test on resuls/fa and limited-offer/fa page

--- app/(custom-layouts)/(no-nav)/limited-offer/[style]/config.ts
+++ app/(custom-layouts)/(no-nav)/limited-offer/[style]/config.ts
@@ -13,6 +13,7 @@
 
   ap: {
     videoSrc: 'rg0tm16tLYQ',
+    videoSrcVariant: '',
     HERO: {
       headline:
         'Are You Ready To Experience Profound Relationships That Give You The Reassurance And Confidence You Need?',
@@ -207,6 +208,7 @@
 
   da: {
     videoSrc: '46LShiTH07c',
+    videoSrcVariant: '',
     HERO: {
       headline:
         'Are You Ready To Experience Relationships That Give You Your Space, Freedom, And Independence?',
@@ -392,6 +394,7 @@
 
   fa: {
     videoSrc: '3xQhjx7pB84',
+    videoSrcVariant: 'bNok_9EEQxo',
     HERO: {
       headline:
         'Are You Ready To Have Deeply Connected Relationships Without Losing Your Freedom And Independence?',
@@ -569,6 +572,7 @@
   },
   sa: {
     videoSrc: 'RBhxl6xR69U',
+    videoSrcVariant: '',
     HERO: {
       headline:
         'Are You Ready To Experience The Balanced, Harmonious, And Fulfilling Relationships That You Deserve?',

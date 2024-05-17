73edac60891cbc46e72e82852e06aeb675afb1b6
rajputpraj <pratikraj@personaldevelopmentschool.com>
2024-05-17 17:18:45 -0400

    GM-1058 page dev

--- app/(custom-layouts)/(no-nav)/attachment-bootcamp/page.tsx
+++ app/(custom-layouts)/(no-nav)/attachment-bootcamp/page.tsx
@@ -9,6 +9,9 @@ import { Button } from '@/components/Button/Button'
 import { List } from '@/components/List'
 import { Faq } from '@/components/Faq/Faq'
 import { CarouselTestimonialThinkific } from '@/components/Carousel/variants/CarouselTestimonialThinkific'
+import { Link } from '@/components/Link'
+import { TrustbarSlider } from '@/components/Trustbar/variants/TrustbarSlider'
+import { Icon } from '@/components/Icon'
 
 export const metadata: Metadata = {
   title: 'Join Our 90-Day Secure Attachment Bootcamp',
@@ -101,6 +104,38 @@ export default function AttachmentBootcampPage() {
           <strong>Stop for a minute and imagine a life where:</strong>
         </p>
 
+        <div className="grid grid-cols-2 gap-4 my-8 lg:grid-cols-5">
+          <div>
+            <Icon name="heart" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>You are confident and worthy of love</p>
+          </div>
+
+          <div>
+            <Icon name="comments" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>You experience deep meaningful relationships</p>
+          </div>
+
+          <div>
+            <Icon name="lock" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>You feel secure in yourself and others</p>
+          </div>
+
+          <div>
+            <Icon name="people-group" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>You are not lonely and have no anxiety or worries</p>
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <Icon name="heart-crack" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>You no longer experience heartache</p>
+          </div>
+        </div>
+
         <p>
           <strong>
             Sounds like a dream, right?
@@ -124,7 +159,7 @@ export default function AttachmentBootcampPage() {
 
         <div className="grid grid-cols-1 gap-4 text-left mb-8 lg:grid-cols-2">
           <List
-            classNameIcon="!text-green-check mt-1 mr-2"
+            classNameIcon="!text-green-check mt-1 mr-4"
             classNameListItems="font-bold mb-4"
             iconName="check-circle"
             iconType="light"
@@ -137,7 +172,7 @@ export default function AttachmentBootcampPage() {
           />
 
           <List
-            classNameIcon="!text-green-check mt-1 mr-2"
+            classNameIcon="!text-green-check mt-1 mr-4"
             classNameListItems="font-bold mb-4"
             iconName="check-circle"
             iconType="light"
@@ -247,6 +282,43 @@ export default function AttachmentBootcampPage() {
       </Section>
 
       <Section className="max-w-5xl mx-auto">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <div>IMAGE</div>
+
+          <div className="text-left mb-8">
+            <h2 className="mb-4">What’s the Cost of Not Taking Action?</h2>
+
+            <List
+              classNameIcon="!text-primary mt-1 mr-4"
+              classNameListItems="mb-4"
+              iconName="face-disappointed"
+              iconType="light"
+              listItems={[
+                'Missing out on the relationships you want',
+                'Believing you don’t deserve true love and connection',
+                'Allowing your core fears to consume you and your relationships',
+                'Letting your self-sabotaging habits ruin any potential happiness',
+                'Never understanding your or your partner’s needs',
+                'Allowing people to cross your boundaries',
+                'Struggle to communicate your emotions and feelings healthily',
+              ]}
+            />
+
+            <p>
+              But you have the chance to take control of your life and relationships with our
+              exclusive 90-Day Attachment Style Bootcamp. Your chance is now:
+            </p>
+          </div>
+        </div>
+
+        <Button label="START YOUR TRANSFORMATION" className="mb-4" />
+
+        <p>
+          <small>*Offer valid for a limited time only. Join now.</small>
+        </p>
+      </Section>
+
+      <Section className="max-w-5xl mx-auto">
         <h2 className="mb-4">Don't Just Take Our Word For It</h2>
 
         <p>
@@ -256,6 +328,246 @@ export default function AttachmentBootcampPage() {
       </Section>
 
       <CarouselTestimonialThinkific />
+
+      <Section className="bg-about-mobile min-h-screen z-15 lg:hidden">
+        <div className="text-black text-left relative z-20 pb-96 lg:col-span-7">
+          <h2 className="mb-4">
+            Meet Your Mentor:
+            <br />
+            Thais Gibson, PhD
+          </h2>
+
+          <p>
+            As the founder of our revolutionary program, Thais Gibson brings a wealth of knowledge,
+            expertise, understanding, and empathy to the table.
+            <br />
+            <br />
+            With a PhD and over 13 different certifications, and co-founder of The Personal
+            Development School, she’s leading people worldwide to change their lives and
+            relationships through their attachment style.
+            <br />
+            <br />
+            She’s featured on many popular podcasts, including The Mel Robbins Podcast. And is
+            launching her brand new podcast soon – The Thais Gibson Podcast – which will include
+            special guests, such as Dr. Caroline Leaf, Dr. Isabella Wentz, Kevin Miller, host of
+            What's Driving You, and Jemma Sbeg, the host of Psychology of Your 20s. You can find her
+            best-selling book,{' '}
+            <Link
+              url="https://www.amazon.com/Learning-Love-Relationships-Integrated-Attachment/dp/B0CFGFV5CH"
+              label="Learning Love"
+            />
+            , on Amazon now, explaining how you can get the best relationships of your life.
+            <br />
+            <br />
+            As part of this exclusive bootcamp, you’ll get to enjoy 1-1 time with Thais in weekly
+            Q&A, giving you the opportunity to ask her any personal questions.
+            <br />
+            <br />
+            It’s what makes this bootcamp so powerful and truly unique: how often do you get to work
+            with a personal development leader?
+          </p>
+        </div>
+      </Section>
+
+      <Section
+        className={`bg-about w-full hidden min-h-80 z-10 lg:block lg:py-16 xl:py-18 2xl:py-20 3xl:py-22`}
+        classNameInner="relative z-10 lg:grid lg:grid-cols-12">
+        <div className="text-black text-left lg:col-span-7">
+          <h2 className="mb-4">
+            Meet Your Mentor:
+            <br />
+            Thais Gibson, PhD
+          </h2>
+
+          <p>
+            As the founder of our revolutionary program, Thais Gibson brings a wealth of knowledge,
+            expertise, understanding, and empathy to the table.
+            <br />
+            <br />
+            With a PhD and over 13 different certifications, and co-founder of The Personal
+            Development School, she’s leading people worldwide to change their lives and
+            relationships through their attachment style.
+            <br />
+            <br />
+            She’s featured on many popular podcasts, including The Mel Robbins Podcast. And is
+            launching her brand new podcast soon – The Thais Gibson Podcast – which will include
+            special guests, such as Dr. Caroline Leaf, Dr. Isabella Wentz, Kevin Miller, host of
+            What's Driving You, and Jemma Sbeg, the host of Psychology of Your 20s. You can find her
+            best-selling book,{' '}
+            <Link
+              url="https://www.amazon.com/Learning-Love-Relationships-Integrated-Attachment/dp/B0CFGFV5CH"
+              label="Learning Love"
+            />
+            , on Amazon now, explaining how you can get the best relationships of your life.
+            <br />
+            <br />
+            As part of this exclusive bootcamp, you’ll get to enjoy 1-1 time with Thais in weekly
+            Q&A, giving you the opportunity to ask her any personal questions.
+            <br />
+            <br />
+            It’s what makes this bootcamp so powerful and truly unique: how often do you get to work
+            with a personal development leader?
+          </p>
+        </div>
+      </Section>
+
+      <TrustbarSlider />
+
+      <Section className="max-w-5xl mx-auto" classNameInner="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <div className="text-left">
+          <h2 className="mb-4">Why We're Your Best Choice for Relationship Transformation</h2>
+
+          <p className="font-bold tracking-33 mb-4">
+            OUR COMMITMENT TO EXCELLENCE IS ESTABLISHED WITH A 99.7% STUDENT SATISFACTION SCORE
+          </p>
+
+          <p>
+            With our 90-Day Attachment Style Bootcamp, you won't find another program with as much
+            ongoing support, life-changing tools, and scientifically proven methods to transform
+            your relationships and become secure:
+          </p>
+        </div>
+
+        <div className="text-left">
+          <List
+            classNameIcon="!text-green-check mt-1 mr-4"
+            classNameListItems="font-bold mb-4"
+            iconName="check-circle"
+            iconType="light"
+            listItems={[
+              'Personalized program tracks tailored to your specific attachment style and goals',
+              '30,000+ people experiencing significant change in their lives thanks to the program',
+              'A stunning 99.7 NPS score (student recommending the program) from students from over 200+ countries',
+              'Groundbreaking research and methodologies rooted in Gibson Integrated Attachment Theory, proven to deliver results in record time',
+              'Daily support in small and large group formats, led by trained and certified counselors and coaches',
+              'A supportive online community of like-minded individuals committed to growth and connection',
+            ]}
+          />
+        </div>
+      </Section>
+
+      <Section className="w-full bg-blue-lightest" classNameInner="max-w-5xl mx-auto">
+        <div className="grid grid-cols-1 gap-8 text-left mb-8 lg:grid-cols-3">
+          <div>
+            <p className="font-bold tracking-33 mb-4">WHY YOU NEED US</p>
+
+            <p>
+              Everyone wants to feel secure and have deeply connective and loving relationships.
+              They just don’t know how to get them.
+              <br />
+              <br />
+              You now have an opportunity that many people don’t: the chance to move past your
+              insecurities and create the love and relationships you desire.
+              <br />
+              <br />
+              This 90-Day Attachment Style Bootcamp will do it for you.
+            </p>
+          </div>
+
+          <div>
+            <p className="font-bold tracking-33 mb-4">WHY WE'RE UNIQUE</p>
+
+            <p>
+              Our game-changing approach goes into the subconscious mind to uncover and reprogram
+              your thoughts, patterns, and beliefs, so you can move past your insecure attachment
+              style and become securely attached.
+              <br />
+              <br />
+              It’s why our students enjoy long-lasting results and positive outcomes and become the
+              best version of themselves.
+              <br />
+              <br />
+              And you can get it all with our 90-Day Attachment Style Bootcamp.
+            </p>
+          </div>
+
+          <div>
+            <p className="font-bold tracking-33 mb-4">WHY WE'RE IN-DEMAND</p>
+
+            <p>
+              By creating the Bootcamp, we’ve combined the perfect formula of learning, empowerment,
+              and encouragement that makes us stand out.
+              <br />
+              <br />
+              Most importantly, we’ve made it affordable and accessible.
+              <br />
+              <br />
+              We’ve never made this offer before for this price or value. It’s a small investment
+              that will lead to secure relationships without breaking the bank.
+              <br />
+              <br />
+              That’s why this program is available for a limited time only.
+            </p>
+          </div>
+        </div>
+
+        <Button label="START YOUR BOOTCAMP NOW" className="mb-8" />
+
+        <div className="max-w-2xl mx-auto">
+          <p>
+            *Pay $149 for the first 90 days. After 90 days, you’ll auto-renew to our All-Access Pass
+            – which gives you access to everything in the school – for $149 per quarter! That’s 25%
+            off the regular price. You can cancel at any time with no commitment.
+          </p>
+        </div>
+      </Section>
+
+      <Section className="max-w-5xl mx-auto">
+        <h2 className="mb-4">You Won’t Be Alone In Your Journey</h2>
+
+        <p className="tracking-33 mb-4">
+          <strong>
+            EMBARKING ON A TRANSFORMATIONAL JOURNEY ALONE CAN BE SCARY AND LONELY WITH A FEAR OF
+            FAILURE.
+          </strong>
+        </p>
+
+        <p>
+          <strong>But with our 90-Day Attachment Style Bootcamp, you’ll constantly:</strong>
+        </p>
+
+        <div className="grid grid-cols-2 gap-4 my-8 lg:grid-cols-5">
+          <div>
+            <Icon name="compass" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>Meet like-minded people who are on their own journey — like you</p>
+          </div>
+
+          <div>
+            <Icon name="heart" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>Make healthy relationships with a thriving community</p>
+          </div>
+
+          <div>
+            <Icon name="people-group" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>Have a welcoming and helpful group to turn to for questions</p>
+          </div>
+
+          <div>
+            <Icon name="hand-heart" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>Have daily support from coaches and peers with support groups</p>
+          </div>
+
+          <div className="col-span-2 lg:col-span-1">
+            <Icon name="messages-question" type="light" size="5x" className="text-primary mb-4" />
+
+            <p>Get personal guidance from Thais Gibson via webinars and Q&As</p>
+          </div>
+        </div>
+
+        <div className="max-w-2xl mx-auto">
+          <p>
+            Our vibrant and empowering community environment will empower you through the hard
+            times, have a shoulder to hold you up, and offer ideas that can spark new habits.
+            <br />
+            <br />
+            You’ll never feel or be alone in your journey.
+          </p>
+        </div>
+      </Section>
     </Page>
   )
 }

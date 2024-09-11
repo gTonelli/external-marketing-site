5ee9e634d5f7d3ce872c0c0f274291d95c603301
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2024-09-11 14:46:14 -0400

    GM-1180 changed lifetime pricing to sale pricing.

--- app/(custom-layouts)/(no-nav)/lifetime/page.tsx
+++ app/(custom-layouts)/(no-nav)/lifetime/page.tsx
@@ -51,7 +51,7 @@ const pricingPlanbenefits: string[] = [
 const pricingPlan: IPricingPlan[] = [
   {
     title: 'ONE TIME PAYMENT',
-    currentPrice: 1799,
+    currentPrice: 1399,
     originalPrice: 2399,
     isRecommended: true,
     url: EExternalRoutes.THINKIFIC_CHECKOUT_LIFETIME_UPFRONT,
@@ -59,7 +59,7 @@ const pricingPlan: IPricingPlan[] = [
   },
   {
     title: '6 MONTH PAYMENT PLAN',
-    currentPrice: 339,
+    currentPrice: 269,
     originalPrice: 449,
     isRecommended: false,
     url: EExternalRoutes.THINKIFIC_CHECKOUT_LIFETIME_6_MONTH_PLAN,
@@ -67,7 +67,7 @@ const pricingPlan: IPricingPlan[] = [
   },
   {
     title: '12 MONTH PAYMENT PLAN',
-    currentPrice: 179,
+    currentPrice: 149,
     originalPrice: 239,
     isRecommended: false,
     url: EExternalRoutes.THINKIFIC_CHECKOUT_LIFETIME_12_MONTH_PLAN,

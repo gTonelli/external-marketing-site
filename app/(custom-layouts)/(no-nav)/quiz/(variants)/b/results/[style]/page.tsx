d77744e8162c39748ee69594679af9c9d8fecc3d
Alpesh Patel <alpesh@personaldevelopmentschool.com>
2024-10-31 10:36:17 -0400

    GM-1242 | add new routes for meta funnel and update control routes for existing mel robbins test

--- app/(custom-layouts)/(no-nav)/quiz/(variants)/b/results/[style]/page.tsx
+++ app/(custom-layouts)/(no-nav)/quiz/(variants)/b/results/[style]/page.tsx
@@ -5,7 +5,7 @@ import { QuizResultsPage } from '../../../../[style]/QuizResultsPage'
 import { TParams  } from '../../../../[style]/page'
 import { FAResultsPage } from '../../../../results/fa/FAResultsPage'
 
-export default function MetaResultPages({ params }: TParams) {
+export default function MetaResultsPage({ params }: TParams) {
   return (
     <Page className="w-full text-center z-10" page_name={`vsl-meta-${params.style}`}>
       {params.style === 'fa' ? <FAResultsPage /> : <QuizResultsPage style={params.style} />}

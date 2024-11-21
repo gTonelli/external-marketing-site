8fa34a8bc9f8339173b83011ee96cf0eedcab0e9
rajputpraj <pratikraj@personaldevelopmentschool.com>
2024-11-21 09:52:48 -0500

    GM-1267 cleanup

--- app/(custom-layouts)/(no-nav)/dream-life/page.tsx
+++ app/(custom-layouts)/(no-nav)/dream-life/page.tsx
@@ -1,61 +1,61 @@
-'use client'
+// core
+import Image from 'next/image'
 //components
-import { Button } from '@/components/Button/Button'
-import { Image } from '@/components/Image'
-import { Text } from '@/components/Text/Text'
 import { TestimonialSection } from '@/components/TestimonialSection'
 import { Page } from '@/components/Page'
-import { Video } from '@/components/Video/Video'
 import { Articles } from '@/components/Articles'
 import { CommunityTeaser } from '@/components/CommunityTeaser'
 import { PaymentOptions } from '@/components/PaymentOptions'
+import VideoTeaser from '@/components/Video/variants/VideoTeaser'
+import { CheckoutButton } from '@/components/CheckoutButton'
 // config
 import { TRIAL_HEADSPACE as TH } from './config'
-// modules
-import Mixpanel from '@/modules/Mixpanel'
 // utils
 import { EExternalRoutes } from '@/utils/constants'
 
 export default function DreamLifePage() {
-  const page_name = '7 Day Free Trial Headspace'
-
-  const onGoToCheckout = (event: React.MouseEvent<Element, MouseEvent>) => {
-    window.location.assign(EExternalRoutes.THINKIFIC_CHECKOUT_7_DAY_TRIAL)
-  }
-
   return (
     <Page className="relative w-full overflow-hidden" page_name="7 Day Free Trial Headspace">
-      {/**HERO SECTION */}
+      {/* HERO SECTION */}
       <section className="w-full text-center bg-grey-7 pt-6 pb-10 px-4 lg:pt-22 lg:pb-0">
         <Image
           className="w-full mx-auto sm:w-3/4 md:w-1/2 lg:hidden"
-          src="TrialHeadspace/hero-left-mockup.png"
+          src="/images/TrialHeadspace/hero-left-mockup.png"
           alt="hero-mockup-left"
+          width={729}
+          height={796}
         />
 
-        <Text.Heading
-          className="max-w-[304px] !text-h1 leading-[50px] mx-auto"
-          content={TH.HERO.heading}
-        />
+        <h1 className="max-w-[304px] leading-[50px] mx-auto">{TH.HERO.heading}</h1>
 
         <div className="relative mt-8 lg:mt-12">
-          <PaymentOptions className="lg:flex-col" placement="top" />
+          <PaymentOptions className="lg:flex-col" />
         </div>
       </section>
 
       <Image
         className="hidden absolute w-1/2 top-20 lg:-left-56 lg:block xl:w-1/3 xl:-left-32 2xl:-left-44"
-        src="TrialHeadspace/hero-left-mockup.png"
+        src="/images/TrialHeadspace/hero-left-mockup.png"
         alt="hero-mockup-left"
+        width={729}
+        height={796}
       />
 
       <Image
         className="hidden absolute w-1/2 lg:-right-56 top-20 lg:block xl:w-1/3 xl:-right-32"
-        src="TrialHeadspace/hero-right-mockup.png"
+        src="/images/TrialHeadspace/hero-right-mockup.png"
         alt="hero-mockup-right"
+        width={656}
+        height={708}
       />
 
-      <Image className="w-full" src="TrialHeadspace/hero-bg.png" alt="hero-mockup" />
+      <Image
+        className="w-full"
+        src="/images/TrialHeadspace/hero-bg.png"
+        alt="hero-mockup"
+        width={1120}
+        height={88}
+      />
 
       {/** DREAMLIFE SECTION  */}
       <section className="pt-4 lg:pt-0">
@@ -66,38 +66,39 @@ export default function DreamLifePage() {
               className="flex row items-center space-x-4 mb-1 mx-auto md:justify-center lg:justify-start lg:space-x-14 lg:mb-12">
               <Image
                 className="w-[150px] h-[150px]"
-                src={`TrialHeadspace/${benefit.image}`}
+                src={`/images/TrialHeadspace/${benefit.image}`}
                 alt="attachment-style-benefits"
+                width={438}
+                height={453}
               />
 
               <div className="flex flex-col space-y-1 lg:flex-row lg:space-x-20">
-                <Text.Heading className="lg:w-56" content={benefit.title} size={3} />
+                <h3 className="lg:w-56">{benefit.title}</h3>
 
-                <Text.Paragraph
-                  className="text-sm md:w-64 lg:w-72 lg:text-base"
-                  content={benefit.copy}
-                />
+                <p className="text-sm md:w-64 lg:w-72 lg:text-base">{benefit.copy}</p>
               </div>
             </div>
           ))}
 
           <div className="flex flex-col items-center space-y-14 mx-4 mt-20 md:text-center lg:flex-row lg:space-x-20 lg:space-y-0 lg:text-left">
             <div>
-              <Text.Heading content={TH.DREAM_LIFE.heading} />
+              <h2>{TH.DREAM_LIFE.heading}</h2>
 
-              <Text.Paragraph className="mt-6 lg:mt-11" content={TH.DREAM_LIFE.copy} />
+              <p className="mt-6 lg:mt-11">{TH.DREAM_LIFE.copy}</p>
 
-              <Button
+              <CheckoutButton
                 className="mt-8 px-6 lg:mt-11"
                 label="TRY FOR FREE"
-                onClick={onGoToCheckout}
+                href={EExternalRoutes.THINKIFIC_CHECKOUT_7_DAY_TRIAL}
               />
             </div>
 
             <Image
               className="md:w-1/2"
-              src="TrialHeadspace/dreamlife-mockup.jpg"
+              src="/images/TrialHeadspace/dreamlife-mockup.jpg"
               alt="dreamlife-mockup"
+              width={1512}
+              height={1173}
             />
           </div>
         </div>
@@ -108,22 +109,21 @@ export default function DreamLifePage() {
         <TestimonialSection />
 
         <div className="text-center">
-          <Text.Heading className="mt-14 lg:mt-22" content="Take a Moment For Yourself" size={1} />
+          <h2 className="mt-14 lg:mt-22">Take a Moment For Yourself</h2>
 
-          <Text.Paragraph
-            className="mt-4"
-            content="Explore some snippets of our most popular courses from each of the categories below!"
-          />
+          <p className="mt-4">
+            Explore some snippets of our most popular courses from each of the categories below!
+          </p>
 
-          <Button
+          <CheckoutButton
             className="text-black bg-blue mt-8"
             label="TRY FOR FREE"
-            onClick={onGoToCheckout}
+            href={EExternalRoutes.THINKIFIC_CHECKOUT_7_DAY_TRIAL}
           />
         </div>
       </section>
 
-      <Video.Teaser />
+      <VideoTeaser />
 
       <Articles />
 
--- components/PaymentOptions.tsx
+++ components/PaymentOptions.tsx
@@ -1,59 +1,48 @@
-'use client'
-
 // components
 import { IDefaultProps } from '.'
-import { Button } from './Button/Button'
-import { Text } from './Text/Text'
+import { CheckoutButton } from './CheckoutButton'
 // config
 import { TRIAL_HEADSPACE as TH } from '@/app/(custom-layouts)/(no-nav)/dream-life/config'
 // libraries
 import cx from 'classnames'
-// modules
-import Mixpanel from '@/modules/Mixpanel'
 // utils
 import { EExternalRoutes } from '@/utils/constants'
 
 interface IPaymentOptionsProps extends IDefaultProps {
-  placement?: string
+  checkoutUrl?: EExternalRoutes
+  placement?: 'top' | 'bottom'
 }
 
-export const PaymentOptions = ({ className, placement = 'top' }: IPaymentOptionsProps) => {
-  const onSubmit = () => {
-    window.location.assign(EExternalRoutes.THINKIFIC_CHECKOUT_7_DAY_TRIAL)
-  }
-
+export const PaymentOptions = ({
+  className,
+  checkoutUrl,
+  placement = 'top',
+}: IPaymentOptionsProps) => {
   return (
     <div className={cx(`flex flex-col space-y-4 lg:justify-between items-center`, className)}>
-      <div
-        className="relative max-w-[474px] flex flex-row items-center space-x-5 text-white bg-primary rounded-[30px] p-6 cursor-pointer lg:px-7 lg:pt-7 lg:pb-5"
-        onClick={onSubmit}>
-        <Text.Paragraph
-          className="absolute -top-5 font-bold text-black bg-blue rounded-10 py-2 px-4"
-          content="BEST VALUE"
-        />
+      <div className="relative max-w-[474px] flex flex-row items-center space-x-5 text-white bg-primary rounded-[30px] p-6 cursor-pointer lg:px-7 lg:pt-7 lg:pb-5">
+        <p className="absolute -top-5 font-bold text-black bg-blue rounded-10 py-2 px-4">
+          BEST VALUE
+        </p>
 
         <div className="text-left">
-          <Text.Paragraph
-            className="font-bold"
-            content={TH.HERO.offer.type}
-            spacing="tracking-0.325"
-          />
+          <p className="tracking-33 font-bold">{TH.HERO.offer.type}</p>
 
-          <Text.Heading className="mt-3" content={TH.HERO.offer.title} />
+          <h2 className="mt-3">{TH.HERO.offer.title}</h2>
 
-          <Text.Paragraph
+          <p
             className={`max-w-[336px] !text-sm mt-4 lg:block ${
               placement === 'bottom' && 'hidden lg:block'
-            }`}
-            content={TH.HERO.offer.copy}
-          />
+            }`}>
+            {TH.HERO.offer.copy}
+          </p>
         </div>
       </div>
 
-      <Button
+      <CheckoutButton
         className="w-44 !font-bold border-none mt-4 lg:mt-10"
         label="TRY FOR FREE"
-        onClick={onSubmit}
+        href={checkoutUrl}
       />
     </div>
   )

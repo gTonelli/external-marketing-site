67b935bf64e33b3a541889a4a2c59e090e3e9284
rajputpraj <pratikraj@personaldevelopmentschool.com>
2025-01-09 17:04:06 -0500

    GM-1348 ended quiz form test for google; restarted it for meta only

--- components/AttachmentQuiz/AttachmentQuizForm.tsx
+++ components/AttachmentQuiz/AttachmentQuizForm.tsx
@@ -12,6 +12,7 @@ import { useGoogleTagManager } from '@/modules/GTM'
 // utils
 import { TStyle } from '@/utils/types'
 import { getSplitTest } from '@/utils/functions'
+import { useEffect, useState } from 'react'
 
 interface IAttachmentQuizFormProps {
   userStyle: TStyle
@@ -29,13 +30,17 @@ export const AttachmentQuizForm = ({
   const funnelytics = useFunnelytics()
   const tagManager = useGoogleTagManager()
   const router = useRouter()
-  const isVariant = getSplitTest({
-    key: 'GM-1327',
-    experimentName: 'Attachment Quiz Form Style',
-    variantRatio: 0.5,
-    useCookies: false,
-  })
-  console.log('isVariant', isVariant)
+  const [isVariant, setIsVariant] = useState(false)
+
+  useEffect(() => {
+    const isFormVariant = getSplitTest({
+      key: 'GM-1348',
+      experimentName: 'GM-1348 Attachment Quiz Form Meta',
+      variantRatio: 0.5,
+      useCookies: false,
+    })
+    setIsVariant(isFormVariant)
+  }, [])
 
   // ==================== Events ====================
   const determineRoute = () => {
@@ -79,7 +84,7 @@ export const AttachmentQuizForm = ({
   return (
     <section className="flex justify-center">
       <div className="max-w-5xl w-full text-center rounded-2xl mt-6 mx-2 p-2 xxs:p-3 xs:p-4 xxs:shadow-centered md:mx-4 md:p-8 lg:px-12 xl:px-16">
-        {isVariant ? (
+        {isVariant && quiz_traffic_source === 'paidMeta' ? (
           <>
             <h2 className="font-bold font-sspb text-center mb-4">
               You're <strong className="text-primary">One Step Away</strong> From Changing Your

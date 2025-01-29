8d7bdb9dc38dba11c4b8392b969a1d34ee0de94e
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2025-01-29 12:23:17 -0600

    PROD-3571 added ip api pro api key.

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -4,14 +4,15 @@ export const runtime = 'edge'
 
 export async function GET(request: NextRequest, response: NextResponse) {
   return fetch(
-    `http://ip-api.com/json/${
+    `https://pro.ip-api.com/json/${
       request.headers.get('x-forwarded-for')?.split(',')[0] || request.ip
-    }?fields=countryCode`
+    }?key=${process.env.NEXT_PRIVATE_IP_API_KEY}&fields=countryCode`
   )
     .then((res) => res.json())
-    .then((data) => {
-      return new NextResponse(JSON.stringify({ countryCode: data.countryCode || 'Unkown' }), {
-        headers: { 'Cache-Control': 'public, max-age=600000' },
-      })
-    })
+    .then(
+      (data) =>
+        new NextResponse(JSON.stringify({ countryCode: data.countryCode || 'Unkown' }), {
+          headers: { 'Cache-Control': 'public, max-age=600000' },
+        })
+    )
 }

50839de2ade8c9c7a74cd26a248cbcf3ee5aa59d
rajputpraj <pratikraj@personaldevelopmentschool.com>
2025-04-22 15:42:42 -0400

    PROD-3901 changed date

--- components/DowntimeBanner.tsx
+++ components/DowntimeBanner.tsx
@@ -13,8 +13,8 @@ export const DowntimeBanner = () => {
 
   useEffect(() => {
     setDowntime(
-      `${dayjs('2025-04-22Z02:00:00-04:00').format('h:mm A')} to ${dayjs(
-        '2025-04-22Z04:00:00-04:00'
+      `${dayjs('2025-04-24Z02:00:00-04:00').format('h:mm A')} to ${dayjs(
+        '2025-04-24Z04:00:00-04:00'
       ).format('h:mm A')}`
     )
   }, [])
@@ -24,10 +24,10 @@ export const DowntimeBanner = () => {
       <FontAwesomeIcon className="!hidden mx-2 lg:!inline-block" icon={faRoadBarrier} />
 
       <p className="!mb-0">
-        Scheduled Maintenance: April 22, {downtime}. Some features will be temporarily unavailable.{' '}
+        Scheduled Maintenance: April 24, {downtime}. Some features will be temporarily unavailable.{' '}
         <Link
           className="underline hover:text-white"
-          href="https://support.personaldevelopmentschool.com/en/articles/11095885-scheduled-downtime-april-7-2025-what-you-need-to-know?_gl=1*sqy61f*_gcl_au*MTI5ODYwOTcxNC4xNzQ0OTA1MzQ4"
+          href="https://support.personaldevelopmentschool.com/en/articles/11095885-scheduled-downtime-april-24-2025-what-you-need-to-know"
           target="_blank">
           Learn more
         </Link>

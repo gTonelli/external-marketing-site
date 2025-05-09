ce9a5857d97edc4b453bc6a2a6d0caff2c8d7492
Graham Tonelli <gtonelli@personaldevelopmentschool.com>
2025-05-09 16:50:49 -0500

    GM-1708 adjusted heading on mha page b.

--- app/(custom-layouts)/(no-nav)/mha-month/page.tsx
+++ app/(custom-layouts)/(no-nav)/mha-month/page.tsx
@@ -19,7 +19,7 @@ export default function MentalHealthAwareness() {
     <Page className="w-full overflow-hidden" page_name="mha-month">
       {/* HEADER TEXT */}
       <Section className="relative z-10 bg-blue-lightest lg:pb-0">
-        <h1 className="leading-10 font-effra font-bold lg:leading-10 lg:max-w-[940px] lg:mx-auto lg:text-5xl lg:mb-8">
+        <h1 className="leading-10 lg:leading-10 lg:max-w-[940px] lg:mx-auto lg:mb-8">
           Tame Your Anxiety, Build Your Self-Esteem, or Break Your Toxic Relationship Patterns
         </h1>
 

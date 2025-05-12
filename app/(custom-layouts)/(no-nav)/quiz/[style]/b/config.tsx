25856f0d0618481bbd513ddb648293a5e17bbb74
rajputpraj <pratikraj@personaldevelopmentschool.com>
2025-05-12 11:14:13 -0400

    GM-1387 imports cleanup

--- app/(custom-layouts)/(no-nav)/quiz/[style]/b/config.tsx
+++ app/(custom-layouts)/(no-nav)/quiz/[style]/b/config.tsx
@@ -1,9 +1,10 @@
+// components
+import { List } from '@/components/List'
 // libraries
+import { faCheckCircle } from '@awesome.me/kit-545b942488/icons/classic/regular'
 import { fa1, fa2, fa3, fa4, fa5 } from '@awesome.me/kit-545b942488/icons/classic/solid'
 // utils
 import { TStyle } from '@/utils/types'
-import { List } from '@/components/List'
-import { faCheckCircle } from '@awesome.me/kit-545b942488/icons/classic/regular'
 
 export const config = {
   base: {

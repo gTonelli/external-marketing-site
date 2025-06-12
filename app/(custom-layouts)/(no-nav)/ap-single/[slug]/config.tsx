873f98edece9887cea556588b1de0dd752b932f6
rajputpraj <pratikraj@personaldevelopmentschool.com>
2025-06-12 10:40:55 -0400

    GM-1785 GM-1743 copy changes to AP single pages

--- app/(custom-layouts)/(no-nav)/ap-single/[slug]/config.tsx
+++ app/(custom-layouts)/(no-nav)/ap-single/[slug]/config.tsx
@@ -3,19 +3,15 @@ export const AP_SINGLE_CONFIG = {
     checkoutUrl: '/limited-offer/ap',
     program: {
       header:
-        'Break Free From Anxiety & Being Overwhelmed in Your Relationships. Get The Love You Deserve',
-      subheader:
-        'THIS PROGRAM IS DESIGNED TO HELP YOU FEEL SECURE, LOVED, AND FREE IN YOUR RELATIONSHIPS',
+        'Get The Love You Deserve In a Relationship Where You’re Secure & Loved. Only With This Life-Changing Program',
+      subheader: '',
       copy: [
         <>
           The <strong>Anxious Preoccupied to Secure Relationship Program</strong> gives you the
           personalized roadmap and revolutionary, practical tools (only available at our school) to
-          break unhealthy patterns, build deeper connections, and thrive in emotionally strong
-          relationships while maintaining your independence and freedom!gives you the personalized
-          roadmap and revolutionary, practical tools (only available at our school) to break free
-          from the endless cycle of worry, doubt, and fear that’s been holding you back. This isn’t
-          just another relationship course—it’s a powerful platform to help you feel safe, secure,
-          and confident in your connections.
+          break free from the endless cycle of worry, doubt, and fear that’s been holding you back.
+          This isn’t just another relationship course—it’s a powerful program to help you feel safe,
+          secure, and confident in your connections.
         </>,
         <>
           It’s part of our BONUS offer: a <strong>30% discount on our All-Access Pass</strong>; our
@@ -23,11 +19,10 @@ export const AP_SINGLE_CONFIG = {
           courses, live webinars and Q&A sessions, study groups, and access to our online community.
         </>,
       ],
-      ctaLabel: 'GET THE LOVE YOU WANT',
+      ctaLabel: 'GET THE PROGRAM NOW',
     },
     benefits: {
-      header:
-        'Experience a True Transformation—Here’s How This Program Will Help You Elevate Your Relationships',
+      header: 'Here’s How This Program Will Help You Elevate Your Relationships',
       subheader: 'LET GO OF ANXIETY, BUILD STRONGER CONNECTIONS, AND FEEL TRULY SEEN AND LOVED',
       listItems: [
         <>
@@ -36,9 +31,9 @@ export const AP_SINGLE_CONFIG = {
           are.
         </>,
         <>
-          <strong>Break Free From the Anxiety Spiral</strong> – Say goodbye to overthinking,
-          rumination, and the constant search for reassurance. Discover simple, proven strategies to
-          quiet your mind and reclaim your emotional peace, and remove emotional rollercoasters.
+          <strong>Break Free From the Anxiety Spiral</strong> – Say goodbye to overthinking and the
+          constant search for reassurance. Discover simple, proven strategies to quiet your mind and
+          reclaim your emotional peace, and remove emotional rollercoasters.
         </>,
         <>
           <strong>Release Old Emotional Baggage</strong> – Let go of the old relationship patterns
@@ -47,13 +42,13 @@ export const AP_SINGLE_CONFIG = {
         </>,
         <>
           <strong>Confidently Communicate & Set Boundaries</strong> – It’s time to stop ignoring
-          your own needs and embrace what you need by speaking with honesty and clarity while
+          your own needs and embrace what you desire by speaking with honesty and clarity while
           setting strong boundaries.
         </>,
         <>
           <strong>Create Lasting Emotional Security</strong> – Build the confidence you need to feel
           secure in your relationship—without the constant fear or emotional chaos of
-          misunderstanding. Feel safe, loved, and respected, all while staying true to yourself.
+          misunderstanding.
         </>,
         <>
           <strong>Embrace Vulnerability Without Being Hurt</strong> – Opening up doesn’t have to
@@ -71,9 +66,9 @@ export const AP_SINGLE_CONFIG = {
           Expect to experience more success and confidence in your dating life and relationships.
         </>
       ),
-      ctaLabel: 'SAY YES TO THE NEW YOU*',
+      ctaLabel: 'GET THE PROGRAM NOW!*',
       disclaimer:
-        '*When you sign up for the course, you’ll become an All-Access Pass Member for $67 per month – a 30% discount! And because I know this course will change your life, I’ll give you a 100% refund if you don’t see a transformation within 7 days – no questions asked!',
+        '*When you sign up for the program, you’ll become an All-Access Pass Member for $67 per month – a 30% discount! And because I know this program will change your life, I’ll give you a 100% refund if you don’t see a transformation within 7 days – no questions asked!',
     },
     askYourself: {
       header: 'The Sooner You Start, The Sooner You’ll Embrace Peace & Progress',
@@ -93,7 +88,7 @@ export const AP_SINGLE_CONFIG = {
           </strong>
         </>,
       ],
-      ctaLabel: 'BECOME SECURE & EMPOWERED',
+      ctaLabel: 'START YOUR TRANSFORMATION',
     },
   },
   versions: {
@@ -102,37 +97,37 @@ export const AP_SINGLE_CONFIG = {
         'Your Abandonment Fears Are Ruining Your Relationships. Watch This Video to Stop Them!',
       subheader:
         'This short video has a 3-part exercise to crush your fear of abandonment at its root. The result? You will finally feel secure, even when someone’s not immediately there.',
-      videoId: '9EjUgp1FCxg',
+      videoId: 'zyRs28ycHcE',
     },
     'overcoming-loneliness': {
       header: 'Your Fear of Being Lonely Can Make Dating Situations Seem Bigger Than Ever.',
       subheader:
         'But that can change. Starting now. Watch this video to uncover how you can self-soothe when panic and loneliness kick in.',
-      videoId: 'BLYAXy1Xf4M',
+      videoId: 'kPv5SuD8Ms4',
     },
     texting: {
       header: 'Waiting for a Text Back Feels Like Emotional Torture.',
       subheader:
         'But here’s the thing: Not everyone texts with the same frequency or emotional weight. This video explains how and why your different texting expectations lead to challenges.',
-      videoId: 'BsLKi8m46ZQ',
+      videoId: 'n5APUUtTL0g',
     },
     'regulate-emotions': {
       header: 'If It’s So Hard For You to Calm Down When You’re Upset, Here’s Why.',
       subheader:
         "When your nervous system gets activated, it stays that way. It’s not because you're too emotional; it’s because you were not taught to self-soothe. This video can help you learn to regulate your emotions.",
-      videoId: 'KwOmBu2agmU',
+      videoId: 'VmUv_KteJxU',
     },
     limerence: {
       header: 'If You’re Always Fantasizing About the Wrong People, It’s the Limerence Trap.',
       subheader:
         'As an Anxious Preoccupied person, it’s easy to fall into limerence traps and become obsessed with a person. The truth? You’re not getting the consistent love and needs met right here and now. The video explains why!',
-      videoId: 'KuVmGpmfAWE',
+      videoId: 'd5nIpmQ9saQ',
     },
     'becoming-secure': {
       header: 'You’re Healing, Even When It Doesn’t Always Feel Like It.',
       subheader:
-        'When you’ve lived most of your life feeling anxious in love, waiting for the other shoe to drop, and fearing abandonment, security can feel unfamiliar. This short video explains the key signs that your anxious attachment style is turning into secure.',
-      videoId: '9eYH3MkO5W8',
+        'When you’ve lived most of your life feeling anxious in love, waiting for the other shoe to drop, and fearing abandonment, security can feel unfamiliar. This short video explains the key signs that your Anxious Preoccupied style is turning into secure.',
+      videoId: 'XzbTU2anFpo',
     },
   },
 }

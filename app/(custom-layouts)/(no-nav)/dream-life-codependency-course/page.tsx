7e46c15c5bacfa9703e81e512c9fa45194e26b28
rajputpraj <pratikraj@personaldevelopmentschool.com>
2025-10-02 14:52:23 -0400

    GM-1988 added codependency page

---
+++ app/(custom-layouts)/(no-nav)/dream-life-codependency-course/page.tsx
@@ -0,0 +1,17 @@
+// core
+import { Metadata } from 'next'
+// components
+import { PromotionPage } from '@/components/FreeTrialFreeCourse/PromotionPage'
+// config
+import { CONFIG } from './config'
+
+export const metadata: Metadata = {
+  title: 'Heal Codependency Patterns & Thrive In Love!',
+  description:
+    'Break free from codependency and reclaim your independence. Our course helps you set healthy boundaries, boost self-worth, and create healthy love.',
+  robots: 'noindex',
+}
+
+export default function DreamLifeCodependencyCoursePage() {
+  return <PromotionPage config={CONFIG} pageName="Dreamlife Codependency Course Page" />
+}

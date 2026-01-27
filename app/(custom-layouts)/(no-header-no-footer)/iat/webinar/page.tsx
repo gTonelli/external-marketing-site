c0ae24386c4347b91cac134ea81a938d4da18a77
Fuzailkhan <fuzail@personaldevelopmentschool.com>
2026-01-27 11:57:05 -0500

    IP-1778: Update masterclass registration date and timer to Feb 18, 5:30PM ET (A/B)

--- app/(custom-layouts)/(no-header-no-footer)/iat/webinar/page.tsx
+++ app/(custom-layouts)/(no-header-no-footer)/iat/webinar/page.tsx
@@ -37,7 +37,7 @@ export default function IATWebinarPage() {
       <section className="bg-purple-dark">
         <div className="default-padding pt-4 lg:pt-8">
           <CountdownTimer
-            date={getOfferEndDate(new Date('2026-01-13T17:30:00-05:00'), 1)}
+            date={getOfferEndDate(new Date('2026-02-18T17:30:00-05:00'), 1)}
             theme="dark"
           />
         </div>

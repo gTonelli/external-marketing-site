cb6faabd4752d7d4cd721f3f01e0357ac1eb2b9b
rajputpraj <pratikraj@personaldevelopmentschool.com>
2026-01-28 15:55:40 -0500

    IP-1779 updated post webinar pages timer

--- app/(custom-layouts)/(no-header-no-footer)/iat/webinar/recording-offer/page.tsx
+++ app/(custom-layouts)/(no-header-no-footer)/iat/webinar/recording-offer/page.tsx
@@ -72,7 +72,7 @@ export default function IATMasterclassRecordingPage() {
         <CountdownTimer
           className="mb-8"
           theme="light"
-          date={getOfferEndDate(new Date('2026-01-17T17:30:00-05:00'), 1)}
+          date={getOfferEndDate(new Date('2026-02-22T17:30:00-05:00'), 1)}
         />
       </Section>
 

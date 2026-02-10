97d6494cf22f688c366041a0ec068eae8d4c31b6
rajputpraj <pratikraj@personaldevelopmentschool.com>
2026-02-10 09:57:54 -0500

    GM-2354 GM-2385 updated attachment bundle report seo

--- app/(custom-layouts)/(unclickable-logo-no-nav-no-footer)/attachment-bundle-report/[style]/page.tsx
+++ app/(custom-layouts)/(unclickable-logo-no-nav-no-footer)/attachment-bundle-report/[style]/page.tsx
@@ -1,11 +1,11 @@
 // core
 import { Metadata } from 'next'
 // components
-import { metadata as _metadata } from '../../attachment-report/[style]/page'
 import { Page } from '@/components/Page'
 import { AttachmentReport } from '../../attachment-report/[style]/AttachmentReport'
 // utils
 import { TStyle } from '@/utils/types'
+import { getAttachmentStyleText } from '@/utils/functions'
 
 export const dynamicParams = false
 
@@ -13,7 +13,19 @@ export function generateStaticParams() {
   return [{ style: 'fa' }, { style: 'ap' }, { style: 'da' }, { style: 'sa' }]
 }
 
-export const metadata: Metadata = _metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ style: TStyle }>
+}): Promise<Metadata> {
+  const { style } = await params
+  const attachmentStyleText = getAttachmentStyleText(style)
+  return {
+    title: `${attachmentStyleText} Attachment Report | PDS`,
+    description: `Your attachment style is ${attachmentStyleText}. Discover key traits, patterns, and growth tools in your personalized report by Thais Gibson.`,
+    robots: 'noindex',
+  }
+}
 
 export default function AttachmentReportPage({ params }: { params: { style: TStyle } }) {
   return (
